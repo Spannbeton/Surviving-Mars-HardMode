421f4d2d7a2c5323c7984fb57b1855e2efa74012
Spannbeton <37778105+Spannbeton@users.noreply.github.com>
2018-04-01 00:25:08 +0200

    Outsourcing Bugfix and Description

--- Realistic Mars (hard gameplay)/Code/No Outsourcing.lua
+++ Realistic Mars (hard gameplay)/Code/No Outsourcing.lua
@@ -3,7 +3,7 @@ g_Consts.OutsourceDisabled = 1
 local ToggleNOO = true
 --- Mod Config Settings
 function OnMsg.NewHour(hour) --failsafe
-ToggleNOO = ModConfig:Get("Hard Mode", "ToggleNoOutsourcing") or true
+ToggleNOO = ModConfig:Get("Hard Mode", "ToggleNoOutsourcing")
 if (ToggleNOO==false) then
 	g_Consts.OutsourceDisabled = 0
 else 
